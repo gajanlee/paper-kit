7d790ee7e608dd38c90470ae5c19e0be23098f30
lee <lee_jiazh@163.com>
2018-09-05 23:46:06 +0800

    save done, next baidu scholor spider

--- scripts/service.py
+++ scripts/service.py
@@ -2,6 +2,7 @@
 
 # This module will expose interface as a RESTful server.
 
+import json
 import os
 import bottle
 from bottle import route, run, request
@@ -11,10 +12,14 @@ files = [path for path in os.listdir(DIR_PATH) if os.path.isfile(os.path.join(DI
 
 app = bottle.Bottle()
 
+# return html page's content, maybe basepath should't in the parameter
+def html_page(path):
+    with open(path) as fp:
+        return fp.read()
+
 @app.get("/")
 def main_page():
-    with open("../pages/tags.html") as fp:
-        return fp.read()
+    return html_page("../pages/tags.html")
 
 @app.get("/files")
 def get_files():
@@ -25,6 +30,14 @@ def get_file_content():
     global files
     with open(os.path.join(DIR_PATH, files[int(request.query.file_id)])) as fp:
         return {"content": fp.read()}
+
+@app.route("/amend", method="PATCH")
+def amend_file():
+    body = json.load(request.body)
+
+    with open(os.path.join(DIR_PATH, files[int(request.query.file_id)]), "w") as fp:
+        fp.write(body["content"])
+        return {"status": "ok"}
 
 if __name__ == "__main__":
     app.run(port=8081, host="0.0.0.0")
